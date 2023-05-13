04eb8ddcec784b39c0ccc05e66cb9dff7ccfe360
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-05-13 13:54:59 +0400

    Initial Commit

---
+++ Nutritionly/NutritionlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionlyApp.swift
+//  Nutritionly
+//
+//  Created by aplle on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritionlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
